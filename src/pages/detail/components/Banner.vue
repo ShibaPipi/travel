f1b9ac338fcef101051033f5ed38f0c69a7a68e4
孙耀鹏 <33478747+ShibaPipi@users.noreply.github.com>
2019-04-09 18:44:44 +0800

    animation finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -10,17 +10,20 @@
         </div>
       </div>
     </div>
-    <common-gallery
-      :imgs="galleryImgs"
-      v-show="showGallery"
-      @close="handleGalleryClose"
-    >
-    </common-gallery>
+    <fade-animation>
+      <common-gallery
+        :imgs="galleryImgs"
+        v-show="showGallery"
+        @close="handleGalleryClose"
+      >
+      </common-gallery>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallery from 'common/gallery/Gallery'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'Banner',
   props: {
@@ -46,7 +49,8 @@ export default {
     }
   },
   components: {
-    CommonGallery
+    CommonGallery,
+    FadeAnimation
   }
 }
 </script>
