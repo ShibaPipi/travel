0aeb22ae0a0f604656a100d072b45c9aca937f1a
孙耀鹏 <33478747+ShibaPipi@users.noreply.github.com>
2019-04-09 15:43:17 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1711/e1/e14af792aca4f7b3a3.water.jpg_600x330_8dd9e7d5.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">天津方特欢乐世界</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe658;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallery
+      :imgs="imgs"
+      v-show="showGallery"
+      @close="handleGalleryClose"
+    >
+    </common-gallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'Banner',
+  data () {
+    return {
+      showGallery: false,
+      imgs: [
+        '//img1.qunarzz.com/sight/p0/1711/4b/4b802bff6ee9df69a3.water.jpg_r_800x800_c9133213.jpg',
+        '//img1.qunarzz.com/sight/p0/1711/74/746c8484c3dfe4dca3.water.jpg_r_800x800_31bcd201.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallery = true
+    },
+    handleGalleryClose () {
+      this.showGallery = false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
